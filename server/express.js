746efcfc3505972bef4cc9aead9f2df25514d72f
stephendoyin <stevedoyin@gmail.com>
2018-11-06 04:39:30 +0100

    initial commit

---
+++ server/express.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+
+
+const CURRENT_WORKING_DIR = process.cwd();
+const app = express();
+
+
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compress());
+// secure apps by setting various HTTP headers
+app.use(helmet());
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+
+// mount routes
+
+
+// Catch unauthorised errors
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  }
+});
+
+export default app;
